f6d34e13cdfc103e4cc4ed054bc85a9a9d837d57
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-07-18 11:40:50 +0900

    Initial commit

---
+++ Source/ProjectRPG/ProjectRPGGameMode.h
@@ -0,0 +1,19 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ProjectRPGGameMode.generated.h"
+
+UCLASS(minimalapi)
+class AProjectRPGGameMode : public AGameModeBase
+{
+	GENERATED_BODY()
+
+public:
+	AProjectRPGGameMode();
+};
+
+
+
