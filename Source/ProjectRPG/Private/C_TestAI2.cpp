31ae7ac0fe75014fc83c6a39dc3afc68c1adb316
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-07-20 23:03:28 +0900

    test : git ai 테스트

--- Source/ProjectRPG/Private/C_TestAI2.cpp
+++ Source/ProjectRPG/Private/C_TestAI2.cpp
@@ -11,7 +11,14 @@ C_TestAI2::C_TestAI2()
 		if (true)
 			a += i;
 	}
+
+	for (int i = 0; i < 100; i++)
+	{
+		if (true)
+			a += i;
+	}
 	UE_LOG(LogTemp, Warning, TEXT("C_TestAI2 created with a value of %d"), a);
+
 }
 
 C_TestAI2::~C_TestAI2()
