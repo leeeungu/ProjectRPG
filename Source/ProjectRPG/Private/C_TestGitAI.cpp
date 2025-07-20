802d80a40c1da60d0f1a1e4a630fb2703409dbb4
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-07-20 19:34:31 +0900

    test : git ai 테스트

--- Source/ProjectRPG/Private/C_TestGitAI.cpp
+++ Source/ProjectRPG/Private/C_TestGitAI.cpp
@@ -5,8 +5,12 @@
 
 C_TestGitAI::C_TestGitAI()
 {
+	int* pNode = new int(5);
+	pNode = new int(5);
 }
 
 C_TestGitAI::~C_TestGitAI()
 {
+	delete pNode; // This will cause a memory leak if not handled properly
+	pNode = nullptr; // Reset pointer to avoid dangling pointer
 }
