69d65ef817f293b32e19efe582e5ee124d852ed6
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-07-20 21:04:51 +0900

    test : git ai 테스트

---
+++ Source/ProjectRPG/Private/C_TestAI2.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "C_TestAI2.h"
+
+C_TestAI2::C_TestAI2()
+{
+}
+
+C_TestAI2::~C_TestAI2()
+{
+}
---
+++ Source/ProjectRPG/Public/C_TestAI2.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+
+/**
+ * 
+ */
+class PROJECTRPG_API C_TestAI2
+{
+public:
+	C_TestAI2();
+	~C_TestAI2();
+};
