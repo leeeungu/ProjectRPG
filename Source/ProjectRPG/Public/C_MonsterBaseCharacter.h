032bd354059bdf8d4533562aff9efeab1722bc50
siyoon1 <122285776+siyoon1@users.noreply.github.com>
2025-07-21 21:18:26 +0900

    feature : 몬스터 베이스 클래스 생성
    C_MonsterBaseCharacter 클래스 생성

---
+++ Source/ProjectRPG/Public/C_MonsterBaseCharacter.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "C_BaseCharacter.h"
+#include "C_MonsterBaseCharacter.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRPG_API AC_MonsterBaseCharacter : public AC_BaseCharacter
+{
+	GENERATED_BODY()
+	
+};
