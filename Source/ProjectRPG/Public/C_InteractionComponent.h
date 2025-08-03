d7f5043bc8b157c9935878afe377b35b7f8b1823
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-08-03 23:20:54 +0900

    feature : 상호 작용 시스템 추가
    - player에 attach할 componenet 추가
    - searching collision과 interface를 interaction 시스템으로 변경
    - collision object 셋팅과 preset 추가

---
+++ Source/ProjectRPG/Public/C_InteractionComponent.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/CapsuleComponent.h"
+#include "C_InteractionComponent.generated.h"
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInteractionEvent, AActor*, pDetectedActor);
+
+UCLASS(ClassGroup = "Collision", meta = (DisplayName = "Interaction", BlueprintSpawnableComponent))
+class PROJECTRPG_API UC_InteractionComponent : public UCapsuleComponent
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(BlueprintAssignable, BlueprintCallable)
+	FOnInteractionEvent m_onInteraction{};
+
+public:
+	UC_InteractionComponent();
+
+	UFUNCTION(BlueprintCallable)
+	bool runInteraction(AActor* pDetectedActor);
+
+	UFUNCTION(BlueprintPure)
+	static FString getInteractionPresetName() { return TEXT("InteractionPreset"); }
+
+	virtual void BeginPlay() override;
+};
