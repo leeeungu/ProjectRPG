586ae78e51812d30d0edfcbd1d8134647132f0f5
siyoon1 <122285776+siyoon1@users.noreply.github.com>
2025-08-14 15:41:56 +0900

    feature : Look 태스크 추가 및 기능 개선
    BT에 Look task 추가
    CheckAttackCondition 서비스에
    공격 체크 조건 개선

--- Source/ProjectRPG/Public/C_BTTaskPatrol.h
+++ Source/ProjectRPG/Public/C_BTTaskPatrol.h
@@ -4,9 +4,6 @@
 
 #include "CoreMinimal.h"
 #include "BehaviorTree/BTTaskNode.h"
-#include "C_BaseCharacter.h"
-#include "C_MonsterAiController.h"
-#include "NavigationSystem.h"
 #include "C_BTTaskPatrol.generated.h"
 
 /**
@@ -18,7 +15,7 @@ class PROJECTRPG_API UC_BTTaskPatrol : public UBTTaskNode
 	GENERATED_BODY()
 
 public:
-	UC_BTTaskPatrol() = default;
+	UC_BTTaskPatrol();
 
 	virtual EBTNodeResult::Type ExecuteTask(UBehaviorTreeComponent& OwnerComp, uint8* NodeMemory) override;
 	
