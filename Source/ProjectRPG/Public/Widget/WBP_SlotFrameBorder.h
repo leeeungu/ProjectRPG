afb342023ae3d6d743b8a703895557d5dd5c722e
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-08-19 00:47:18 +0900

    docs : widget  수정

---
+++ Source/ProjectRPG/Public/Widget/WBP_SlotFrameBorder.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Components/Border.h"
+#include "WBP_SlotFrameBorder.generated.h"
+
+
+UCLASS(Blueprintable)
+class PROJECTRPG_API UWBP_SlotFrameBorder : public UBorder
+{
+	GENERATED_BODY()
+
+public:
+	UWBP_SlotFrameBorder();
+};
