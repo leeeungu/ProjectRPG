30eafd3c58a1c2592ca790b7804cada422b4504a
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-08-21 13:28:02 +0900

    test : 기능 수정 없이 테스트

--- Source/ProjectRPG/Private/Notify/C_SphereAttackNotify.cpp
+++ Source/ProjectRPG/Private/Notify/C_SphereAttackNotify.cpp
@@ -2,10 +2,29 @@
 #include "Components/SkeletalMeshComponent.h"
 #include "BlueprintFunctionLibrary/C_AttackRayCasting.h"
 #include "C_BaseCharacter.h"
+#include "Debug/DebugDrawService.h"
+#include "Engine/SkeletalMeshSocket.h"
+
+DEFINE_LOG_CATEGORY_STATIC(C_SphereAttackNotify, Log, All);
+
+void UC_SphereAttackNotify::OnAnimNotifyCreatedInEditor(FAnimNotifyEvent& ContainingAnimNotifyEvent)
+{
+	//ContainingAnimNotifyEvent.
+}
 
 void UC_SphereAttackNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
 {
+
 	UAnimNotify::Notify(MeshComp, Animation, EventReference);
-	UC_AttackRayCasting::attackSphereTrace(MeshComp->GetOwner(), m_sPositionfloat, m_strSocketName, m_fRadius, m_fDamage,
-		m_bDrawDebug);
+#if WITH_EDITOR
+	if (m_bDrawDebug && MeshComp && MeshComp->GetSocketByName(m_strSocketName))
+	{
+		FVector Position = m_sPosition;
+		Position += MeshComp->GetSocketByName(m_strSocketName)->GetSocketLocation(MeshComp);
+		if (GetWorld())
+			DrawDebugSphere(GetWorld(), m_sPosition, m_fRadius, 0, FColor::Purple, false, 1.0f);
+	}
+#elif
+	UC_AttackRayCasting::attackSphereTrace(MeshComp->GetOwner(), m_sPosition, m_strSocketName, m_fRadius, m_fDamage, m_bDrawDebug);
+#endif
 }
--- Source/ProjectRPG/Public/Notify/C_SphereAttackNotify.h
+++ Source/ProjectRPG/Public/Notify/C_SphereAttackNotify.h
@@ -10,7 +10,7 @@ class PROJECTRPG_API UC_SphereAttackNotify : public UAnimNotify
 	GENERATED_BODY()
 protected:
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "UC_SphereAttackNotify")
-	FVector m_sPositionfloat{};
+	FVector m_sPosition{};
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "UC_SphereAttackNotify")
 	FName m_strSocketName = NAME_None;
 	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "UC_SphereAttackNotify")
@@ -21,6 +21,6 @@ protected:
 	bool m_bDrawDebug = false;
 
 private:
-
+	virtual void OnAnimNotifyCreatedInEditor(FAnimNotifyEvent& ContainingAnimNotifyEvent) override;
 	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
 };
