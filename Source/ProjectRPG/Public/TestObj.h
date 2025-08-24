38bce7ac05e70593398f4419068536764df988d2
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-08-24 15:43:20 +0900

    Test

---
+++ Source/ProjectRPG/Public/TestObj.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "TestObj.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECTRPG_API UTestObj : public UObject
+{
+	GENERATED_BODY()
+	
+};
