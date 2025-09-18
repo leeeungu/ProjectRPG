4923230b1f879a73ff36ca56a77e0d9a67e8c050
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-09-18 14:12:48 +0900

    feature : 몬스터 공격 노티파이 추가

---
+++ Source/ProjectRPG/Private/Monster/C_MonsterAttackNotify.cpp
@@ -0,0 +1,51 @@
+﻿#include "Monster/C_MonsterAttackNotify.h"
+#include "C_BaseCharacter.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
+#include "Debug/DebugDrawService.h"
+
+void UC_MonsterAttackNotify::Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference)
+{
+	UAnimNotify::Notify(MeshComp, Animation, EventReference);
+	if (!MeshComp->GetOwner())
+		return; 
+	
+	if (m_bDrawDebug)
+	{
+		const FTransform MeshTransform = MeshComp->GetSocketTransform(m_SocketName);
+		DrawDebugSphere(MeshComp->GetWorld(), MeshTransform.TransformPosition(m_sRelativePosition), m_Radius, 0, FColor::Purple, false, 1.0f);
+	}
+	//}
+	AC_BaseCharacter* Character = Cast<AC_BaseCharacter>(MeshComp->GetOwner());
+	if (!Character)
+	{
+		if (!Character)
+			UE_LOG(LogTemp, Error, TEXT("UC_AttackRayCasting::attackSphereTrace : pSelf is null"));
+		if (!Cast<AC_BaseCharacter>(Character))
+			UE_LOG(LogTemp, Error, TEXT("UC_AttackRayCasting::attackSphereTrace : pSelf not AC_BaseCharacter"));
+		return ;
+	}
+	const FTransform MeshTransform = MeshComp->GetSocketTransform(m_SocketName);
+	FVector Position = MeshTransform.TransformPosition(m_sRelativePosition);
+	TArray<FHitResult> OutHits{};
+	FCollisionShape CollisionShape = FCollisionShape::MakeSphere(m_Radius);
+	FCollisionQueryParams Params{};
+	Params.AddIgnoredActor(Character);
+	//UE_LOG(AttackBFL, Error, TEXT("UC_AttackRayCasting::attackSphereTrace : AC_BaseCharacter"));
+
+	Character->GetWorld()->SweepMultiByChannel(OutHits, Position, Position, FQuat::Identity, ECollisionChannel::ECC_Visibility
+		, CollisionShape, Params);
+	bool bResult{};
+	for (FHitResult& Hit : OutHits)
+	{
+		AC_BaseCharacter* pCharacter = Cast< AC_BaseCharacter>(Hit.GetActor());
+		if (pCharacter)
+		{
+			bResult = true;
+			pCharacter->takeDamageEvent(m_Damage* pCharacter->getAtk());
+		}
+	}
+	//const FTransform MeshTransform = MeshComp->GetSocketTransform(SocketName);
+	//ReturnComp = UNiagaraFunctionLibrary::SpawnSystemAtLocation(MeshComp->GetWorld(), Template, MeshTransform.TransformPosition(LocationOffset), (MeshTransform.GetRotation() * RotationOffsetQuat).Rotator(), FVector(1.0f), true);
+	//}
+}
---
+++ Source/ProjectRPG/Public/Monster/C_MonsterAttackNotify.h
@@ -0,0 +1,24 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Animation/AnimNotifies/AnimNotify.h"
+#include "C_MonsterAttackNotify.generated.h"
+
+UCLASS()
+class PROJECTRPG_API UC_MonsterAttackNotify : public UAnimNotify
+{
+	GENERATED_BODY()
+protected:
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	FVector m_sRelativePosition{};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MonsterAttackNotify", meta = (AnimNotifyBoneName = "true"))
+	FName m_SocketName{};
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float m_Radius = 10.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	float m_Damage = 1.0f;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite)
+	bool m_bDrawDebug{};
+private:
+	virtual void Notify(USkeletalMeshComponent* MeshComp, UAnimSequenceBase* Animation, const FAnimNotifyEventReference& EventReference) override;
+};
