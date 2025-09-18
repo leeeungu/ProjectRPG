1a54bdb65f31188dd4b236de1e1591f47bca21ac
leeeungu <74060680+leeeungu@users.noreply.github.com>
2025-09-18 10:49:22 +0900

    feature : Damage 위젯 추가
    - 위젯의 루트 슬롯에 damage 가 가하졌을 때 textblock을 생성하거나 재활용 하여 스크린에 render
    - widgecomponent의 자손에서 changehp에 bind 하여 구현
    - onchangehp가 boradcast 되고 damage가 0보다 크면 초록 글씨로 호출 아니면 빨강(C_DamageWidgetComponent )참고
    -

--- Source/ProjectRPG/Private/GamePlay/C_DamageWidget.cpp
+++ Source/ProjectRPG/Private/GamePlay/C_DamageWidget.cpp
@@ -7,19 +7,23 @@
 void UC_DamageWidget::drawDamage(float Damage, FColor Color)
 {
 	int nSize = m_arDamageText.Num();
-	for (int i = 0; i < nSize; i++)
+	UTextBlock* pText{};
+	for (int i = 0; i < nSize && m_arDamageText[i] && m_arDamageText[i]->GetVisibility() == ESlateVisibility::Hidden; i++)
 	{
-		if (m_arDamageText[i] && m_arDamageText[i]->GetVisibility() == ESlateVisibility::Hidden)
-		{
-			drawDamage(m_arDamageText[i], Damage, Color);
-		}
+		pText = m_arDamageText[i];
 	}
+
+	if (!pText)
+		pText = createDataTextBlock();
+	drawDamage(pText, Damage, Color);
 }
 
 void UC_DamageWidget::NativeOnInitialized()
 {
 	UUserWidget::NativeOnInitialized();
 	m_arDamageText.Reset(0);
+	m_arDamageText.Reserve(10);
+	m_arDamageTime.Reserve(10);
 	TArray<UWidget*> ChildWidget;
 	WidgetTree->GetAllWidgets(ChildWidget);
 	for (UWidget*& pWidget : ChildWidget)
@@ -27,34 +31,59 @@ void UC_DamageWidget::NativeOnInitialized()
 		if (UTextBlock* pTextBlock = Cast<UTextBlock>(pWidget))
 		{
 			m_arDamageText.Add(pTextBlock);
-			pTextBlock->SetVisibility(ESlateVisibility::Hidden);
+			InitSetting(pTextBlock);
+			m_arDamageTime.Add(0);
+		}
+	}
+	if (!m_pCanvasPanel)
+	{
+		m_pCanvasPanel = WidgetTree->ConstructWidget<UCanvasPanel>(UCanvasPanel::StaticClass());
+		WidgetTree->RootWidget = m_pCanvasPanel;
+		//rootw
+	}
+}
+
+void UC_DamageWidget::NativeTick(const FGeometry& MyGeometry, float InDeltaTime)
+{
+	UUserWidget::NativeTick(MyGeometry, InDeltaTime);
+	//m_arDamageText
+	int nSize = m_arDamageText.Num();
+	UTextBlock* pText{};
+	for (int i = 0; i < nSize; i++)
+	{
+		if (m_arDamageTime[i] >= m_fUpDistance && m_arDamageText[i])
+		{
+			endText(m_arDamageText[i]);
+			m_arDamageTime[i] = 0;
+		}
+		if (m_arDamageText[i] && m_arDamageText[i]->GetVisibility() != ESlateVisibility::Hidden)
+		{
+			float Distance = InDeltaTime * m_fUpSpeed;
+			if (m_arDamageTime[i] + Distance > m_fUpDistance)
+			{
+				Distance = m_fUpDistance - m_arDamageTime[i];
+			}
+			m_arDamageTime[i] += Distance;
+			UCanvasPanelSlot* CanvasSlot = Cast< UCanvasPanelSlot>(m_arDamageText[i]->Slot);
+			if (CanvasSlot)
+			{
+				CanvasSlot->SetPosition(CanvasSlot->GetPosition() + FVector2D(0, -Distance));
+			}
 		}
 	}
 }
 
-void UC_DamageWidget::NativePreConstruct()
-{
-	UUserWidget::NativePreConstruct();
-	
-}
-
-void UC_DamageWidget::NativeConstruct()
-{
-	UUserWidget::NativeConstruct();
-}
-
-void UC_DamageWidget::NativeDestruct()
-{
-	UUserWidget::NativeDestruct();
-}
-
 UTextBlock* UC_DamageWidget::createDataTextBlock()
 {
+	if (!m_pCanvasPanel)
+	{
+		UE_LOG(LogTemp, Error, TEXT("Widget's CanvasPanel is nullptr"));
+		return nullptr;
+	}
 	UTextBlock* pTextBlock = WidgetTree->ConstructWidget<UTextBlock>(UTextBlock::StaticClass());
-	UCanvasPanelSlot* CanvasSlot = Cast< UCanvasPanelSlot>(m_pCanvasPanel->AddChild(pTextBlock));
-	if (CanvasSlot)
-		CanvasSlot->SetAnchors(FAnchors(0.5f, 0.5f));
-	pTextBlock->SetJustification(ETextJustify::Center);
+	InitSetting(pTextBlock);
+	m_arDamageText.Add(pTextBlock);
+	m_arDamageTime.Add(0);
 	return pTextBlock;
 }
 
@@ -62,7 +91,33 @@ void UC_DamageWidget::drawDamage(UTextBlock* TextBlock, float Damage, FColor Col
 {
 	if (!TextBlock)
 		return;
+	m_pCanvasPanel->AddChild(TextBlock);
+	UCanvasPanelSlot* CanvasSlot = Cast< UCanvasPanelSlot>(TextBlock->Slot);
+	if (CanvasSlot)
+	{
+		CanvasSlot->SetAnchors(FAnchors(0.5f, 0.5f));
+		CanvasSlot->SetPosition(FVector2D(FMath::FRandRange(-10.f,10.f), 0));
+	}
 	TextBlock->SetText(FText::FromString(FString::Printf(TEXT("%d"), (int)Damage)));
 	TextBlock->SetColorAndOpacity(FSlateColor(Color));
 	TextBlock->SetVisibility(ESlateVisibility::SelfHitTestInvisible);
+}
+
+void UC_DamageWidget::InitSetting(UTextBlock* TextBlock)
+{
+	if (!TextBlock)
+		return;
+	FSlateFontInfo Info = TextBlock->GetFont();
+	Info.OutlineSettings.OutlineSize = 2;
+	TextBlock->SetFont(Info);
+	TextBlock->SetJustification(ETextJustify::Center);
+	TextBlock->SetVisibility(ESlateVisibility::Hidden);
+}
+
+void UC_DamageWidget::endText(UTextBlock* TextBlock)
+{
+	if (!TextBlock)
+		return; 
+	TextBlock->SetVisibility(ESlateVisibility::Hidden);
+	TextBlock->RemoveFromParent();
 }
--- Source/ProjectRPG/Public/GamePlay/C_DamageWidgetComponent.h
+++ Source/ProjectRPG/Public/GamePlay/C_DamageWidgetComponent.h
@@ -4,9 +4,20 @@
 #include "Components/WidgetComponent.h"
 #include "C_DamageWidgetComponent.generated.h"
 
-UCLASS()
+class UC_DamageWidget;
+
+UCLASS(ClassGroup = (Damage), meta = (BlueprintSpawnableComponent))
 class PROJECTRPG_API UC_DamageWidgetComponent : public UWidgetComponent
 {
 	GENERATED_BODY()
-	
+private:
+	UC_DamageWidget* m_pDamageWidget{};
+public:
+	UC_DamageWidgetComponent(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	virtual void BeginPlay() override;
+	virtual void OnRegister() override;
+private:
+	UFUNCTION()
+	void changeHp(float fPre, float fCurrent);
+
 };
